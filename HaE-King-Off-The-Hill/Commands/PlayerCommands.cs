b898d2c46250fbc7de25d70802a663454e5b038a
sirhamsteralot <sirhamsteralot@gmail.com>
2024-10-28 21:26:08 +0100

    Add project files.

---
+++ HaE-King-Off-The-Hill/Commands/PlayerCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch.Commands;
+
+namespace HaE_King_Off_The_Hill.Commands
+{
+    [Category("koth")]
+    public class PlayerCommands : CommandModule
+    {
+
+    }
+}
---
+++ HaE-King-Off-The-Hill/KingOffTheHill.cs
@@ -0,0 +1,26 @@
+﻿using HaE_King_Off_The_Hill.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Torch;
+using Torch.API.Plugins;
+
+namespace HaE_King_Off_The_Hill
+{
+    public class KingOffTheHill : TorchPluginBase, IWpfPlugin
+    {
+        private TorchConfigurationUC _torchConfiguration = null;
+        public UserControl GetControl()
+        {
+            if (_torchConfiguration == null)
+            {
+                _torchConfiguration = new TorchConfigurationUC();
+            }
+
+            return _torchConfiguration;
+        }
+    }
+}
