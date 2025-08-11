2dae469d7897273fe1c1adbe9962f89df71128e8
sirhamsteralot <sirhamsteralot@gmail.com>
2025-08-11 18:13:17 +0200

    add testing for the pointscounter

--- KOTH-Tests/InitializationTests.cs
+++ KOTH-Tests/InitializationTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using HaE_King_Off_The_Hill.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace KOTH_Tests
@@ -10,6 +11,12 @@ namespace KOTH_Tests
         public void Should_Equal_HelloWorld()
         {
             Assert.AreEqual("Hello World!", "Hello World!");
+        }
+
+        [TestMethod]
+        public void Initialize_PointCounter()
+        {
+            PointCounter pointCounter = new PointCounter();
         }
     }
 }
---
+++ KOTH-Tests/PointCounterTests.cs
@@ -0,0 +1,70 @@
+﻿using HaE_King_Off_The_Hill.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOTH_Tests
+{
+    [TestClass]
+    public class PointCounterTests
+    {
+        [TestMethod]
+        public void PointCounter_Regular_Add_Value()
+        {
+            const int POINTSTOADD = 10;
+            const int POINTSEXPECTED = 10;
+
+            PointCounter pointCounter = new PointCounter();
+
+            pointCounter.AddScore(POINTSTOADD);
+
+            Assert.AreEqual(pointCounter.Points, POINTSEXPECTED);
+        }
+
+        [TestMethod]
+        public void PointCounter_Regular_Add_Value_Multiple()
+        {
+            const int POINTSTOADD = 10;
+            const int POINTSLOOP = 10;
+            const int POINTSEXPECTED = 100;
+
+            PointCounter pointCounter = new PointCounter();
+
+            for (int i = 0; i < POINTSLOOP; i++)
+                pointCounter.AddScore(POINTSTOADD);
+
+            Assert.AreEqual(pointCounter.Points, POINTSEXPECTED);
+        }
+
+        [TestMethod]
+        public void PointCounter_Regular_Reduce_Value_Under_0()
+        {
+            const int POINTSTOADD = -10;
+            const int POINTSEXPECTED = 0;
+
+            PointCounter pointCounter = new PointCounter();
+
+            pointCounter.AddScore(POINTSTOADD);
+
+            Assert.AreEqual(pointCounter.Points, POINTSEXPECTED);
+        }
+
+        [TestMethod]
+        public void PointCounter_Regular_Reduce_Value_Under_0_From_Points()
+        {
+            const int INITIALPOINTS = 5;
+            const int POINTSTOADD = -10;
+            const int POINTSEXPECTED = 0;
+
+            PointCounter pointCounter = new PointCounter();
+
+            pointCounter.AddScore(INITIALPOINTS);
+            pointCounter.AddScore(POINTSTOADD);
+
+            Assert.AreEqual(pointCounter.Points, POINTSEXPECTED);
+        }
+    }
+}
